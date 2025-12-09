4243fbdeb3abf9956a3793007ffa511f2dd93163
Satya Panuganti <satyapanuganti@gmail.com>
2025-12-09 14:21:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2863729872-e7b4d",
+  "appId": "1:57763131800:web:a55d7d6d1ad378fb8a7bed",
+  "apiKey": "AIzaSyAJ58lLECN1kBpvdGsyvDLizPrcM9F-h14",
+  "authDomain": "studio-2863729872-e7b4d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "57763131800"
+};
